c1fdd3a558b86d4208cbdb6d9d21167914ae4bbf
SoHanDz <aquatic0514@gmail.com>
2025-02-21 18:21:33 +0700

    Sửa Layout và Post

--- src/Components/Layout.tsx
+++ src/Components/Layout.tsx
@@ -1,23 +1,20 @@
 import React from 'react';
 import Sidebar from './Sidebar';
 import Follow_Sidebar from './Follow_Sidebar';
-import Post from './Post';
-import NewPost from './New_Post';
+
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
-const Layout: React.FC<LayoutProps> = ({ children }) => {
-  return (
-    <div className="min-h-screen bg-[#080A0B] text-white">
-<Sidebar />
-<main className="md:ml-64 md:mr-64 p-4">
-  {children}
-</main>
-<Follow_Sidebar />
-    </div>
-  );
-};
+const Layout: React.FC<LayoutProps> = ({ children }) => (
+  <div className="min-h-screen bg-[#080A0B] text-white">
+    <Sidebar />
+    <main className="md:ml-64 md:mr-64 p-4">
+      {children}
+    </main>
+    <Follow_Sidebar />
+  </div>
+);
 
 export default Layout;
