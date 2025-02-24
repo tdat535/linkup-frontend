9e018b1ac54391523926fdbd2e3da2fa473fe593
buingoclinh461 <lbui9621@gmail.com>
2025-02-24 10:52:48 +0700

    thêm giao diên tin nhăn

--- src/Components/Layout.tsx
+++ src/Components/Layout.tsx
@@ -1,21 +1,24 @@
-import React from 'react';
-import { Outlet } from "react-router-dom";
-import Sidebar from './Sidebar';
-import Follow_Sidebar from './Follow_Sidebar';
+import React from "react";
+import { Outlet, useLocation } from "react-router-dom";
+import Sidebar from "./Sidebar";
+import Follow_Sidebar from "./Follow_Sidebar";
 
 const Layout: React.FC = () => {
+  const location = useLocation();
+  const hideFollowSidebar = location.pathname === "/messages";
+
   return (
     <div className="min-h-screen bg-[#080A0B] text-white flex">
       {/* Sidebar bên trái */}
       <Sidebar />
 
       {/* Nội dung chính */}
-      <main className="flex-1 md:ml-64 md:mr-64 ">
+      <main className={`flex-1 md:ml-64 ${hideFollowSidebar ? "md:mr-0" : "md:mr-64"}`}>
         <Outlet /> {/* Đây là nơi các trang sẽ hiển thị */}
       </main>
 
-      {/* Sidebar bên phải */}
-      <Follow_Sidebar />
+      {/* Sidebar bên phải (ẩn khi ở trang /messages) */}
+      {!hideFollowSidebar && <Follow_Sidebar />}
     </div>
   );
 };
