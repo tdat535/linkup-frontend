d174239d96d586ecce309b8cf11203718edd3dc0
buingoclinh461 <lbui9621@gmail.com>
2025-02-25 08:43:02 +0700

    them nut dang bai

--- src/Components/Sidebar.tsx
+++ src/Components/Sidebar.tsx
@@ -57,10 +57,11 @@ const Sidebar = () => {
 
             <Link to="/more" className="flex py-2 px-4 rounded hover:bg-gray-600">
                 <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="white" viewBox="0 0 24 24">
-                    <path stroke="currentColor" strokeLinecap="round" strokeWidth="2" d="M6 12h.01m6 0h.01m5.99 0h.01"/>
+                    <path stroke="white" strokeLinecap="round" strokeWidth="2" d="M6 12h.01m6 0h.01m5.99 0h.01"/>
                 </svg>
                 <p className='ml-2'>Thêm</p>
             </Link>
+            <button className="w-full bg-blue-500 p-2 rounded-full hover:bg-blue-700 cursor-pointer transition mt-10">Đăng bài</button>
           </nav>
         </div>
         
