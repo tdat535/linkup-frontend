8d349d739d01873e81769b49ac0a6461beb443fb
SoHanDz <aquatic0514@gmail.com>
2025-03-04 09:18:17 +0700

    cố test api follow

--- src/Components/Layout/Sidebar.tsx
+++ src/Components/Layout/Sidebar.tsx
@@ -51,11 +51,11 @@ const Sidebar = () => {
                 <p className='ml-2'>Trang cá nhân</p>
             </Link>
 
-            <Link to="/home/about" className="flex py-2 px-4 rounded hover:bg-gray-600">
+            <Link to="/home/follow-test" className="flex py-2 px-4 rounded hover:bg-gray-600">
                 <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="white" viewBox="0 0 24 24">
                     <path fillRule="evenodd" d="M4 4a1 1 0 0 1 1-1h14a1 1 0 1 1 0 2v14a1 1 0 1 1 0 2H5a1 1 0 1 1 0-2V5a1 1 0 0 1-1-1Zm5 2a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1h1a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1H9Zm5 0a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1h1a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1h-1Zm-5 4a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1h1a1 1 0 0 0 1-1v-1a1 1 0 0 0-1-1H9Zm5 0a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1h1a1 1 0 0 0 1-1v-1a1 1 0 0 0-1-1h-1Zm-3 4a2 2 0 0 0-2 2v3h2v-3h2v3h2v-3a2 2 0 0 0-2-2h-2Z" clipRule="evenodd"/>
                 </svg>
-                <p className='ml-2'>About Us</p>
+                <p className='ml-2'>FollowTest</p>
             </Link>
 
             <Link to="/home/more" className="flex py-2 px-4 rounded hover:bg-gray-600">
