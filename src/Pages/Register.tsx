15fb59d74f8e31fa1549a5863f5754a386c7506a
Tạ Minh Dũng <136634915+taminhdung@users.noreply.github.com>
2025-03-07 11:47:59 +0700

    new

--- src/Pages/Register.tsx
+++ src/Pages/Register.tsx
@@ -2,7 +2,6 @@ import 'flowbite';
 import { useNavigate } from 'react-router-dom';
 import background from '../assets/pictures/images.jpg';
 import { useState } from 'react';
-import Button from '../Components/Buttons/Button';
 
 const Register = () => {
     const navigate = useNavigate();
@@ -78,42 +77,37 @@ const Register = () => {
 
     return (
         <div className="flex justify-center items-center min-h-screen bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url(${background})` }}>
-            <div className="flex flex-col md:flex-row w-full bg-opacity-50">
-                <div className="div-left flex flex-col justify-center items-center md:w-1/2 w-full md:min-h-screen h-auto p-6">
-                    <div className="text-5xl w-full ps-16 break-words text-white">𝓛𝓲𝓷𝓴𝓤𝓹</div>
-                    <div className="mt-4 w-full text-xl ps-16 break-words text-white hidden md:block">
-                        Kết nối với bạn bè và chia sẻ những khoảnh khắc vui vẻ cùng nhau thông qua LinkUp
-                    </div>
-                </div>
-                <div className="div-right flex justify-center items-center p-6 md:w-1/2 w-full md:min-h-screen h-auto">
-                    <form onSubmit={handleSubmit} className="max-w-sm mx-auto p-6 border rounded-2xl border-stone-800 w-full bg-black bg-opacity-75" style={{ maxWidth: '32rem', height: '40rem' }}>
-                        {error && <div className="text-red-500 text-sm mb-3">{error}</div>}
+            <div className="flex-col md:flex-row w-full bg-opacity-50 p-5">
+                <div className='text-5xl text-center break-words text-white'>𝓛𝓲𝓷𝓴𝓤𝓹</div>
+                <div className="flex justify-center items-center w-full mt-10">
+                    <form onSubmit={handleSubmit} className="max-w-sm mx-auto p-6 border rounded-2xl border-stone-800 w-full bg-black bg-opacity-75 shadow-[3px_3px_0px_rgba(100,100,100,0.3)]" style={{ maxWidth: '32rem', height: '40rem' }}>
+                        {<p className='text-red-500 text-sm mb-3'>{error || "ㅤ"}</p>}
                         <div className="mb-3">
                             <label className="block mb-2 text-sm font-medium text-white">Tên tài khoản</label>
-                            <input type="text" name="username" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value={formData.username} onChange={handleChange} required />
+                            <input type="text" name="username" className="bg-black border border-white text-white placeholder-gray-700 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 focus:focus:bg-[rgb(232,240,254)] focus:border-black focus:text-black" value={formData.username} onChange={handleChange} required />
                         </div>
                         <div className="mb-3">
                             <label className="block mb-2 text-sm font-medium text-white">Số điện thoại</label>
-                            <input type="text" name="phonenumber" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value={formData.phonenumber} onChange={handleChange} required />
+                            <input type="text" name="phonenumber" className="bg-black border border-white text-white placeholder-gray-700 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 focus:focus:bg-[rgb(232,240,254)] focus:border-black focus:text-black" value={formData.phonenumber} onChange={handleChange} required />
                         </div>
                         <div className="mb-3">
                             <label className="block mb-2 text-sm font-medium text-white">Email</label>
-                            <input type="email" name="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value={formData.email} onChange={handleChange} required />
+                            <input type="email" name="email" className="bg-black border border-white text-white placeholder-gray-700 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 focus:focus:bg-[rgb(232,240,254)] focus:border-black focus:text-black" value={formData.email} onChange={handleChange} required />
                         </div>
                         <div className="mb-3">
                             <label className="block mb-2 text-sm font-medium text-white">Mật khẩu</label>
-                            <input type="password" name="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value={formData.password} onChange={handleChange} required />
+                            <input type="password" name="password" className="bg-black border border-white text-white placeholder-gray-700 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 focus:bg-[rgb(232,240,254)] focus:border-black focus:text-black" value={formData.password} onChange={handleChange} required />
                         </div>
                         <div className="mb-3">
                             <label className="block mb-2 text-sm font-medium text-white">Nhập lại mật khẩu</label>
-                            <input type="password" name="confirmPassword" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value={formData.confirmPassword} onChange={handleChange} required />
+                            <input type="password" name="confirmPassword" className="bg-black border border-white text-white placeholder-gray-700 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 focus:focus:bg-[rgb(232,240,254)] focus:border-black focus:text-black" value={formData.confirmPassword} onChange={handleChange} required />
                         </div>
                         
-                        <button type="submit" className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" disabled={loading}>
+                        <button type="submit" className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" disabled={loading} onKeyDown={(e) => e.key === "Enter" && Register}>
                             {loading ? 'Đang đăng ký...' : 'Đăng ký'}
                         </button>
 
-                        <div className="flex items-center justify-center">
+                        <div className="flex items-center justify-center mt-10">
                             <button onClick={() => navigate('/login')} className="btn-back text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">
                                 Quay lại trang đăng nhập
                             </button>
