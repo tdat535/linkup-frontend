7f51c7bc227780a0bc495a949e3dc47316db553e
tdat <datnguyen4804@gmail.com>
2025-04-08 22:49:38 +0700

    deloy8

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,9 +8,4 @@ export default defineConfig({
     port: 3000,
   },
   plugins: [react(), tailwindcss(),],
-  resolve: {
-    alias: {
-      '@Components': path.resolve(__dirname, './src/Components')
-    }
-  }
 })
