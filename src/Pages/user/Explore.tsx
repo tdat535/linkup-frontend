1064a6b8d64d17a837e09e147c034897fbf267be
tdat <datnguyen4804@gmail.com>
2025-04-09 10:49:18 +0700

    Fix: rename folder to correct casing for Linux

--- src/Pages/user/Explore.tsx
+++ src/Pages/user/Explore.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Search from '@Components/Layout/Search/Search';
+import Search from 'Components/Layout/Search/Search';
 
 const Explore: React.FC = () => {
   return (
